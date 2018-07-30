482adb431d20c392787834583753c5db525ee493
Jedidiah Weller <jedi@webjunto.com>
2018-07-30 07:31:53 -0400

    perf(firebase): add firebase script for specific headers
    not finished yet, work in progress

--- sw.js
+++ sw.js
@@ -1,21 +1,30 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
 
-self.workbox.skipWaiting();
-self.workbox.clientsClaim();
-
 // your custom service worker code
 self.workbox.routing.registerRoute(
-    /\.(?:png|gif|jpg|jpeg|svg)$/,
-    workbox.strategies.cacheFirst({
-      cacheName: 'images',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-        }),
-      ],
-    }),
-);
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  self.workbox.strategies.cacheFirst({
+    plugins: [
+      new workbox.expiration.Plugin({
+        cacheName: 'images',
+        maxEntries: 80,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+); 
+
+self.workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  self.workbox.strategies.cacheFirst({
+    cacheName: 'google-fonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+); 
 
 self.workbox.routing.registerRoute(
     /\.(?:js|css)$/,
@@ -23,5 +32,8 @@ self.workbox.routing.registerRoute(
     cacheName: 'static-resources',
     }),
 );
+
+//Offline analytics for PWA
+self.workbox.googleAnalytics.initialize(); 
 
 self.workbox.precaching.precacheAndRoute([]);
